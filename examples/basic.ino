2eac1743067b623bd5e0b59a61a5c007abdefd75
kannyd <kannyd@gmail.com>
2020-02-06 05:34:29 +0700

    First Commit

---
+++ examples/basic.ino
@@ -0,0 +1,35 @@
+#define BLYNK_PRINT Serial // Comment this out to disable prints and save space
+#include <BlynkSimpleEsp8266.h>
+
+#include "KnWiFiManager.h"
+KnWiFiManager kn;
+
+void setup()
+{
+    Serial.begin(115200);
+    kn.setConfigPortalTimeout(300);
+    kn.autoConnect();
+
+    Blynk.config(kn.getblynkToken(), kn.getblynkServer(), kn.getblynkPort());
+
+    kn.on("/a", handle_a, "Tambahan Menu 1");
+    kn.on("/b", handle_b, "Tambahan Menu 2");
+}
+
+void loop()
+{
+    kn.run();
+    Blynk.run();
+}
+
+void handle_a()
+{
+    String page = "<br/>iki page AAAAAA";
+    kn.send(200, "text/html", page);
+}
+
+void handle_b()
+{
+    String page = "<br/>iki page Bee";
+    kn.send(200, "text/html", page);
+}
